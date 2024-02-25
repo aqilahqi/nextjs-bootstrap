acafefc3472005339e1040d1d260b14261f4beaa
Aqilah <aqilahfatinfauzi@gmail.com>
2024-02-25 14:26:13 +0800

    completed brands section

---
+++ src/app/components/AppBrand.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const AppBrand = (props: any) => {
+  const { brands } = props;
+  return (
+    <div className='py-5 brands-section'>
+      <div className='container'>
+        <p className='text-center text-light-1 fw-bold mb-4 pb-2'>
+          Who we work with
+        </p>
+        <div className='d-flex flex-wrap justify-content-around justify-content-lg-between'>
+          {brands.length > 0 &&
+            brands.map((brand: string) => (
+              <img
+                src={`${brand}`}
+                className='brands img-fluid mb-3'
+                alt='image'
+              />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppBrand;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import Banner from './components/BannerHome';
+import Brands from './components/AppBrand';
 
 const page = () => {
+  const brands = [
+    '/images/microsoft-logo.png',
+    '/images/oracle-logo.png',
+    '/images/atlassian-logo.png',
+    '/images/cloudfare-logo.png',
+    '/images/vmware-logo.png',
+  ];
   return (
     <div>
       <div>
@@ -10,6 +18,7 @@ const page = () => {
           subtitle='Software Recruitment Specialists'
           image='/images/banner-image-1.png'
         />
+        <Brands brands={brands} />
       </div>
     </div>
   );
